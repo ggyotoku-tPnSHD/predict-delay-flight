60dd1a6dd12e51c2d960e58216020171d26bdfd2
ggyotoku-tPnSHD <ggyotoku@gmail.com>
2026-07-30 19:35:36 -0700

    created concurrent file logic

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/akito/predict_delay_flight_portfolio/.venv
+set -gx VIRTUAL_ENV /home/akito/predict-delay-flight/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
